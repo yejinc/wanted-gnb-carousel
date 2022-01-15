d8090f07d8667946d6464b1d5f096c950e704ec9
Jinnie Cho <yejincho1105@gmail.com>
2022-01-15 21:18:51 +0900

    💄 Styled navigators button modules

---
+++ src/components/modules/Modules.js
@@ -0,0 +1,16 @@
+function Modules() {
+  return (
+    <div className="Modules">
+      <div className="navigators">
+        <div className="prevBtn" aria-label="이전 배너로 넘기기 버트">
+          <img src="/assets/icons/arrow-left.svg" alt="to previous banner" />
+        </div>
+        <div className="nextBtn" aria-label="다음 배너로 넘기기">
+          <img src="/assets/icons/arrow-right.svg" alt="to next banner" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Modules

4a8763c3f912eea1802ae6a8fb9a74bc3476766c
Jinnie Cho <yejincho1105@gmail.com>
2022-01-15 21:27:16 +0900

    ♿️ Renamed alternative text of img elements in navitagors

--- src/components/modules/Modules.js
+++ src/components/modules/Modules.js
@@ -3,10 +3,10 @@ function Modules() {
     <div className="Modules">
       <div className="navigators">
         <div className="prevBtn" aria-label="이전 배너로 넘기기 버트">
-          <img src="/assets/icons/arrow-left.svg" alt="to previous banner" />
+          <img src="/assets/icons/arrow-left.svg" alt="left arrow" />
         </div>
         <div className="nextBtn" aria-label="다음 배너로 넘기기">
-          <img src="/assets/icons/arrow-right.svg" alt="to next banner" />
+          <img src="/assets/icons/arrow-right.svg" alt="right arrow" />
         </div>
       </div>
     </div>

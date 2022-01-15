f16517e196f33e8aecbd611c66f92408001fe289
Jinnie Cho <yejincho1105@gmail.com>
2022-01-15 23:00:46 +0900

    💄 Styled avatar module

--- src/components/modules/Modules.js
+++ src/components/modules/Modules.js
@@ -9,6 +9,27 @@ function Modules() {
           <img src="/assets/icons/arrow-right.svg" alt="right arrow" />
         </div>
       </div>
+
+      <li className="moreUser">
+        <button
+          className="profileBtn"
+          type="button"
+          aria-label="프로필 사진 버튼"
+        >
+          <div className="avatarBorder">
+            <div className="avatarDefault">
+              <img
+                src="/assets/images/profile-default.png"
+                alt="기본 프로필 사진"
+              />
+            </div>
+          </div>
+        </button>
+
+        <strong className="profileNew" aria-label="새 알림이 있습니다">
+          <img src="/assets/icons/new.svg" alt="나의 새 알람 아이콘" />
+        </strong>
+      </li>
     </div>
   )
 }

7e73180f6cd5b6a5e760bd98af731708ce0316b5
Jinnie Cho <yejincho1105@gmail.com>
2022-01-15 23:01:27 +0900

    ♿️ Renamed alternative text of img element of prevBtn and nextBtn in navigators

--- src/components/modules/Modules.js
+++ src/components/modules/Modules.js
@@ -3,10 +3,10 @@ function Modules() {
     <div className="Modules">
       <div className="navigators">
         <div className="prevBtn" aria-label="이전 배너로 넘기기 버트">
-          <img src="/assets/icons/arrow-left.svg" alt="left arrow" />
+          <img src="/assets/icons/arrow-left.svg" alt="왼쪽 화살표 아이콘" />
         </div>
         <div className="nextBtn" aria-label="다음 배너로 넘기기">
-          <img src="/assets/icons/arrow-right.svg" alt="right arrow" />
+          <img src="/assets/icons/arrow-right.svg" alt="오른쪽 화살표 아이콘" />
         </div>
       </div>
 

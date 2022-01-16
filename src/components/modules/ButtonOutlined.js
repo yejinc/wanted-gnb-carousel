a54e512447fb7afaaa6a33d5478f9fe7b8f1d6c2
Jinnie Cho <yejincho1105@gmail.com>
2022-01-16 19:06:08 +0900

    🎨 Splitted Buttons component

---
+++ src/components/modules/ButtonOutlined.js
@@ -0,0 +1,13 @@
+function Buttons() {
+  return (
+    <li className="moreDashboard">
+      <button className="btnOutlined" type="button" aria-label="">
+        <a href="/dashboard" alt="기업 서비스 대시보드로 가기">
+          기업 서비스
+        </a>
+      </button>
+    </li>
+  )
+}
+
+export default Buttons

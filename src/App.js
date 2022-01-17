56dd032b7cba320a31dea528c2ac99995860f96e
Jinnie Cho <yejincho1105@gmail.com>
2022-01-17 12:53:41 +0900

    ✨ Created TopCarousel boilerplate and imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import GNB from './components/GNB'
+import TopCarousel from './components/TopCarousel'
 
 function App() {
-  return <GNB />
+  return (
+    <div className="App">
+      <header className="global-header">
+        <GNB />
+      </header>
+
+      <main className="main">
+        <TopCarousel />
+      </main>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/TopCarousel.js
@@ -0,0 +1,4 @@
+function Carousel() {
+  return <div className="carousel"></div>
+}
+export default Carousel

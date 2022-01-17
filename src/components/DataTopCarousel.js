ff203427e8ec17a0bd749d36b302c0c1c77dc7aa
Jinnie Cho <yejincho1105@gmail.com>
2022-01-17 19:17:29 +0900

    🍱 Added TopCarousel component-related data

---
+++ src/components/DataTopCarousel.js
@@ -0,0 +1,84 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    id: 0,
+    title: '성과를 내는 마케팅',
+    desc: '실제 사례를 공개합니다!',
+    imgSM:
+      'https://static.wanted.co.kr/images/banners/1486/fba2df30.thumb_1006_280.jpg',
+    imgMD: 'https://static.wanted.co.kr/images/banners/1486/fba2df30.jpg',
+    url: 'https://www.wanted.co.kr/events/wantedcon24',
+  },
+  {
+    id: 1,
+    title: '해, 커리어 EP 02 공개',
+    desc: '최종 발표를 위한 마지막 관문 2라운드의 승자는?',
+    imgSM:
+      'https://static.wanted.co.kr/images/banners/1468/3df61cbc.thumb_1006_280.jpg',
+    imgMD: 'https://static.wanted.co.kr/images/banners/1468/3df61cbc.jpg',
+    url: 'https://www.youtube.com/watch?v=VLiACiB2lXw&ab_channel=tvNDSTUDIO',
+  },
+  {
+    id: 2,
+    title: '나도 개발자 되고 싶다',
+    desc: '프론트엔드 무료 교육과정 참여하기',
+    imgSM:
+      'https://static.wanted.co.kr/images/banners/1473/41f7b36e.thumb_1006_280.jpg',
+    imgMD: 'https://static.wanted.co.kr/images/banners/1473/41f7b36e.jpg',
+    url: 'https://www.wanted.co.kr/events/pre_onboarding_course_6',
+  },
+  {
+    id: 3,
+    title: 'UX 디자이너의 커리어 설계',
+    desc: '브랜드 가치를 더하는 디자인',
+    imgSM:
+      'https://static.wanted.co.kr/images/banners/1460/619f3af7.thumb_1006_280.jpg',
+    imgMD: 'https://static.wanted.co.kr/images/banners/1460/619f3af7.jpg',
+    url: 'https://www.wanted.co.kr/events/21_12_s08_b01',
+  },
+  {
+    id: 4,
+    title: '개발자 성장 비결 공개!',
+    desc: '글 쓰는 개발자들의 이야기',
+    imgSM:
+      'https://static.wanted.co.kr/images/banners/1484/b2853456.thumb_1006_280.jpg',
+    imgMD: 'https://static.wanted.co.kr/images/banners/1484/b2853456.jpg',
+    url: 'https://www.wanted.co.kr/events/velog01',
+  },
+  {
+    id: 5,
+    title: '성장하는 개발자가 되려면?',
+    desc: 'OOO 검색하지 말 것!',
+    imgSM:
+      'https://static.wanted.co.kr/images/banners/1486/fba2df30.thumb_1006_280.jpg',
+    imgMD: 'https://static.wanted.co.kr/images/banners/1486/fba2df30.jpg',
+    url: 'https://www.wanted.co.kr/events/livetalk28',
+  },
+  {
+    id: 6,
+    title: '마케팅 주니어를 찾습니다',
+    desc: '기업 과제 풀고 취업까지 한번에!',
+    imgSM:
+      'https://static.wanted.co.kr/images/banners/1490/0b775035.thumb_1006_280.jpg',
+    imgMD: 'https://static.wanted.co.kr/images/banners/1490/0b775035.jpg',
+    url: 'https://www.wanted.co.kr/events/pre_onboarding_course_7',
+  },
+  {
+    id: 7,
+    title: '2022년 달라지는 노동법령',
+    desc: "노동법 '채용' 시리즈",
+    imgSM:
+      'https://static.wanted.co.kr/images/banners/1489/312a0c29.thumb_1006_280.jpg',
+    imgMD: 'https://static.wanted.co.kr/images/banners/1489/312a0c29.jpg',
+    url: 'https://www.wanted.co.kr/events/hrclass220106',
+  },
+  {
+    id: 8,
+    title: '포트폴리오를 부탁해!',
+    desc: '디자이너의 포폴 살펴보기',
+    imgSM:
+      'https://static.wanted.co.kr/images/banners/1487/0d36f0b5.thumb_1006_280.jpg',
+    imgMD: 'https://static.wanted.co.kr/images/banners/1487/0d36f0b5.jpg',
+    url: 'https://www.wanted.co.kr/events/portfolio_contents_29cm',
+  },
+]

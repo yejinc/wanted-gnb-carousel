3a8827ecf422a30aa5d368427d16b711932e03ee
Jinnie Cho <yejincho1105@gmail.com>
2022-01-19 04:26:56 +0900

    🎨 Splitted Navigators into ButtonPrevNav and ButtonNextNav

---
+++ src/components/modules/ButtonPrevNav.js
@@ -0,0 +1,11 @@
+function ButtonPrevNav() {
+  return (
+    <div className="ButtonPrevNav xxl-only">
+      <div className="prevBtn" aria-label="이전 배너로 넘기기 버튼">
+        <img src="/assets/icons/arrow-left.svg" alt="왼쪽 화살표 아이콘" />
+      </div>
+    </div>
+  )
+}
+
+export default ButtonPrevNav

3c1334f70fdb143da028d934424f7e76292294db
Jinnie Cho <yejincho1105@gmail.com>
2022-01-19 08:08:18 +0900

    🐛 Aligned ButtonsPreNav and ButtonNextNav by flex

--- src/components/modules/ButtonNextNav.js
+++ src/components/modules/ButtonNextNav.js
@@ -1,7 +1,15 @@
-function ButtonNextNav() {
+function ButtonNextNav(props) {
+  const { onClick } = props
+
   return (
     <div className="ButtonNextNav xxl-only">
-      <button type="button" className="nextBtn" aria-label="다음 배너로 넘기기">
+      <button
+        type="button"
+        className="nextBtn"
+        aria-label="다음 배너로 넘기기"
+        style={{ display: 'flex', opacity: 0.5 }}
+        onClick={onClick}
+      >
         <img src="/assets/icons/arrow-right.svg" alt="오른쪽 화살표 아이콘" />
       </button>
     </div>
--- src/components/modules/ButtonPrevNav.js
+++ src/components/modules/ButtonPrevNav.js
@@ -1,10 +1,14 @@
-function ButtonPrevNav() {
+function ButtonPrevNav(props) {
+  const { onClick } = props
+
   return (
     <div className="ButtonPrevNav xxl-only">
       <button
         type="button"
         className="prevBtn"
         aria-label="이전 배너로 넘기기 버튼"
+        style={{ display: 'flex', opacity: 0.5 }}
+        onClick={onClick}
       >
         <img src="/assets/icons/arrow-left.svg" alt="왼쪽 화살표 아이콘" />
       </button>

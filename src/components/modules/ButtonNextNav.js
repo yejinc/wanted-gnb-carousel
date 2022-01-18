b023b8e03a35230260e87f4508a5ca9981c0bfdf
Jinnie Cho <yejincho1105@gmail.com>
2022-01-19 04:53:59 +0900

    🐛 Modified div tag to button

--- src/components/modules/ButtonNextNav.js
+++ src/components/modules/ButtonNextNav.js
@@ -1,9 +1,9 @@
 function ButtonNextNav() {
   return (
     <div className="ButtonNextNav xxl-only">
-      <div className="nextBtn" aria-label="다음 배너로 넘기기">
+      <button type="button" className="nextBtn" aria-label="다음 배너로 넘기기">
         <img src="/assets/icons/arrow-right.svg" alt="오른쪽 화살표 아이콘" />
-      </div>
+      </button>
     </div>
   )
 }
--- src/components/modules/ButtonPrevNav.js
+++ src/components/modules/ButtonPrevNav.js
@@ -1,9 +1,13 @@
 function ButtonPrevNav() {
   return (
     <div className="ButtonPrevNav xxl-only">
-      <div className="prevBtn" aria-label="이전 배너로 넘기기 버튼">
+      <button
+        type="button"
+        className="prevBtn"
+        aria-label="이전 배너로 넘기기 버튼"
+      >
         <img src="/assets/icons/arrow-left.svg" alt="왼쪽 화살표 아이콘" />
-      </div>
+      </button>
     </div>
   )
 }
